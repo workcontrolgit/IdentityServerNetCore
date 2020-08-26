57793ae21cf62bfa0fe8c4c6b109d9a3ece7798c
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-08-25 23:50:30 -0400

    Add Azure AD.  Move Azure AD settings to secrets file

--- 04. PART-3 IdentityServer4 ASP.NET Core Identity/src/IdentityServer/Startup.cs
+++ 04. PART-3 IdentityServer4 ASP.NET Core Identity/src/IdentityServer/Startup.cs
@@ -47,6 +47,26 @@ namespace IdentityServer
                 .AddEntityFrameworkStores<IdentityDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddAuthentication()
+                .AddOpenIdConnect("azuread", "Azure AD", options => Configuration.Bind("AzureAd", options));
+
+            services.Configure<OpenIdConnectOptions>("azuread", options =>
+            {
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnRedirectToIdentityProviderForSignOut = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect("/Account/Logout");
+                        return Task.FromResult(0);
+                    }
+                };
+            });
+
+
+
             var builder = services.AddIdentityServer(options =>
             {
                 options.Events.RaiseErrorEvents = true;
